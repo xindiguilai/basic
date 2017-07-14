462c2bbca0b6e2e7c47eec97153f74247e890095
xindiguilai <13311484509@163.com>
2017-07-14 00:04:26 -0700

    about修改

--- views/site/about.php
+++ views/site/about.php
@@ -4,14 +4,14 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = '关于我们';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+	这是关于页面的。您可以修改以下文件来自定义它的内容：
     </p>
 
     <code><?= __FILE__ ?></code>
